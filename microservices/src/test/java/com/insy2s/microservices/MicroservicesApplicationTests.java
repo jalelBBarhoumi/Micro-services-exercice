06d5b115bffd4403cfa25c706d203fd6ac5d9451
Jalel Barhoumi <93652036+JalelBarhoumi@users.noreply.github.com>
2023-02-27 09:18:09 +0100

    first_commit

---
+++ microservices/src/test/java/com/insy2s/microservices/MicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insy2s.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
